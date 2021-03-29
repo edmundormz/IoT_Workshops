ef807b455cc3360cd48143b4f320c6d7c6e6bdb7
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-03-29 11:42:56 -0600

    Adding new script
    This is the initial version of a script to use the ultrasonic sensor (to measure distance) and the DHT11 sensor (to read temperature and moisture)

---
+++ 4_Ultrasonic_DHT11/4_Ultrasonic_DHT11.ino
@@ -0,0 +1,41 @@
+#include <NewPing.h>
+#include <DHT.h>
+ 
+#define TRIGGER_PIN 22
+#define ECHO_PIN 23
+#define MAX_DISTANCE 200
+
+#define DHTPIN 32
+#define DHTTYPE DHT11
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+   Serial.begin(9600);
+   dht.begin();
+}
+ 
+void loop() {
+   delay(1000);
+   
+   unsigned int distance = sonar.ping_cm();
+   Serial.print(distance);
+   Serial.println("cm");
+
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   if (isnan(h) || isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+   }
+   float hic = dht.computeHeatIndex(t,h,false);
+   Serial.print("Humidity: ");
+   Serial.print(h);
+   Serial.print("% ,Temperature: ");
+   Serial.print(t);
+   Serial.print("°C ");
+   Serial.print("Heat index: ");
+   Serial.print(hic);
+   Serial.println("°C");
+}
