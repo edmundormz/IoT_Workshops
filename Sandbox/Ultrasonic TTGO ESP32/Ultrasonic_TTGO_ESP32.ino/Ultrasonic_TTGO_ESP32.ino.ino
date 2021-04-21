934ff9f11cac192a73a798e5c216d2a785b514af
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-04-21 17:45:33 -0500

    TTGO ESP32 board using HC-SR04 sensor
    This works. The solution was to power the ultrasonic sensor with 5V instead of 3.3V

---
+++ Sandbox/Ultrasonic TTGO ESP32/Ultrasonic_TTGO_ESP32.ino/Ultrasonic_TTGO_ESP32.ino.ino
@@ -0,0 +1,42 @@
+#include <NewPing.h>
+#include <DHT.h>
+ 
+#define TRIGGER_PIN 36
+#define ECHO_PIN 39
+#define MAX_DISTANCE 200
+
+#define DHTPIN 25
+#define DHTTYPE DHT11
+
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+   Serial.begin(115200);
+   dht.begin();
+}
+ 
+void loop() {
+   delay(2000);
+   
+   //unsigned int distance = sonar.ping_cm();
+   //Serial.print(distance);
+   Serial.print(sonar.ping_cm());
+   Serial.println("cm");
+
+   float h = dht.readHumidity();
+   float t = dht.readTemperature();
+   if (isnan(h) || isnan(t)){
+    Serial.println("Failed to read from DHT sensor!");
+   }
+   float hic = dht.computeHeatIndex(t,h,false);
+   Serial.print("Humidity: ");
+   Serial.print(h);
+   Serial.print("% ,Temperature: ");
+   Serial.print(t);
+   Serial.print("°C ");
+   Serial.print("Heat index: ");
+   Serial.print(hic);
+   Serial.println("°C");
+}
