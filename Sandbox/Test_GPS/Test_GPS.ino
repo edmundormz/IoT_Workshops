817424fcfad45ac2eb5d658e724efd864339c398
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-05-12 01:46:24 -0500

    Using TinyGPS++ library
    This script uses the TiniGPS++ library to add several features

--- Sandbox/Test_GPS/Test_GPS.ino
+++ Sandbox/Test_GPS/Test_GPS.ino
@@ -1,4 +1,7 @@
+//https://randomnerdtutorials.com/guide-to-neo-6m-gps-module-with-arduino/
+#include <TinyGPS++.h>
 
+TinyGPSPlus gps;
 void setup(){
   Serial.begin(9600);
   //Serial2.begin(9600);
@@ -8,7 +11,24 @@ void setup(){
 void loop(){
   while (Serial2.available() > 0){
     // get the byte data from the GPS
-    byte gpsData = Serial2.read();
-    Serial.write(gpsData);
+    gps.encode(Serial2.read());
+    if (gps.location.isUpdated()){
+      Serial.print("Latitude= "); 
+      Serial.print(gps.location.lat(), 6);
+      Serial.print(" Longitude= "); 
+      Serial.println(gps.location.lng(), 6);
+      // Raw date in DDMMYY format (u32)
+      Serial.print("Raw date DDMMYY = ");
+      Serial.println(gps.date.value());
+      // Raw time in HHMMSSCC format (u32)
+      Serial.print("Raw time in HHMMSSCC = "); 
+      Serial.println(gps.time.value());
+      // Speed in kilometers per hour (double)
+      Serial.print("Speed in km/h = "); 
+      Serial.println(gps.speed.kmph());
+      // Number of satellites in use (u32)
+      Serial.print("Number os satellites in use = "); 
+      Serial.println(gps.satellites.value()); 
+    }
   }
 }
