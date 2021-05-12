d38480c4fe89c6bbde21753b6c3b77416244d947
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-05-12 16:29:13 -0500

    Updated to send SMS
    This version is updated to send info via SMS. Some modifications are required:
    1)Change the trigger pin of ultrasonic_sensor_2 to another pin. It may be 36, 2 or any other not used already by te code
    2)Add your code implemented to read the second ultrasonic sensor using the variable name "cant_latas_sonar_2". Note that the original "cant_latas_sonar" changed to "cant_latas_sonar_1"
    
    Several changes were made:
    a)"Serial" was modified to "SerialMon".  Nothing additional is required
    b)A new variable "door_is" was added. Nothing additional is required
    
    This code executes only once in order to avoid infinite SMS sendings. To re-execute the code just press the reset button in the TTGO board. Changes will be implemented after this version works perfectly, to send SMS only when the door is opened or by a timer (each hour, for instance)

--- Sandbox/Ctrl_Inventarios/Ctrl_Inventarios.ino
+++ Sandbox/Ctrl_Inventarios/Ctrl_Inventarios.ino
@@ -1,20 +1,16 @@
-/// Libraries
-// DS18B20 OneWire Temperature Sensor Libraries
-#include <OneWire.h>
-#include <DallasTemperature.h>
-// HC-SR04 Ultrasonic Sensors Library
-#include <NewPing.h>
-
 /// Global Variables
 // GPIO where the DS18B20 temperature sensor is connected to
 const int oneWireBus = 15;
 // Ultrasonic sensors pins  
 #define TRIGGER_PIN_1 32
 #define ECHO_PIN_1 33
-#define TRIGGER_PIN_2 27
+#define TRIGGER_PIN_2 27 // ***CAMBIAR POR OTRO, PUEDE SER 36,2 
 #define ECHO_PIN_2 14
+//Temperature sensor variable
+float temperatureC;
 // Door sensor (push button)
 int door_state = 0;
+String door_is;
 const int door_pin = 35;  // the number of the door sensor pin
 const int led_pin =  0;    // the number of the indicator LED pin
 // Push buttons array for can lanes
@@ -22,8 +18,53 @@ const int cans_lane_1 = 34; // pin where lane 1 is connected to
 const int cans_lane_2 = 39; // pin where lane 2 is connected to
 int switches_value = 0;
 int cant_latas = 4;
-int cant_latas_sonar = 0;
+int cant_latas_sonar_1 = 0;
+int cant_latas_sonar_2 = 0;
 int dist_latas = 0;
+//GSM modem definitions
+const char simPIN[]   = "1111";     //SIM Card Pin
+#define SMS_TARGET  "+523314173023" //Target phone number
+#define TINY_GSM_MODEM_SIM800      // Modem is SIM800
+#define TINY_GSM_RX_BUFFER   1024  // Set RX buffer to 1Kb
+// TTGO T-Call pins (Do not use this pins for any other thing)
+#define MODEM_RST            5
+#define MODEM_PWKEY          4
+#define MODEM_POWER_ON       23
+#define MODEM_TX             27
+#define MODEM_RX             26
+#define I2C_SDA              21
+#define I2C_SCL              22
+
+
+/// Libraries
+// DS18B20 OneWire Temperature Sensor Libraries
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// HC-SR04 Ultrasonic Sensors Library
+#include <NewPing.h>
+// GSM Modem libraries
+#include <TinyGsmClient.h>  //GSM library
+#include <Wire.h>           //Intended to use with power save function
+
+///GSM Serial debug definitions (to be removed)
+// Set serial for debug console (to Serial Monitor, default speed 115200)
+#define SerialMon Serial
+// Set serial for AT commands (to SIM800 module)
+#define SerialAT  Serial1
+
+/// This is supposed to work as a battery saver when using GSM (To be Studied)
+#define IP5306_ADDR          0x75
+#define IP5306_REG_SYS_CTL0  0x00
+bool setPowerBoostKeepOn(int en){
+  Wire.beginTransmission(IP5306_ADDR);
+  Wire.write(IP5306_REG_SYS_CTL0);
+  if (en) {
+    Wire.write(0x37); // Set bit1: 1 enable 0 disable boost keep on
+  } else {
+    Wire.write(0x35); // 0x37 is default reg value
+  }
+  return Wire.endTransmission() == 0;
+}
 
 /// Instances Declarations
 // OneWire protocol instance and temperature sensor declaration
@@ -32,68 +73,99 @@ DallasTemperature tmp_sensor(&oneWire);
 // Ultrasonic Sensors 
 NewPing sonar_1(TRIGGER_PIN_1, ECHO_PIN_1);
 NewPing sonar_2(TRIGGER_PIN_2, ECHO_PIN_2);
+TinyGsm modem(SerialAT);
 
 /// Configuraciones Iniciales
 void setup() {
-  Serial.begin(115200); // Serial 
+  SerialMon.begin(115200); // SerialMon 
   tmp_sensor.begin(); // Start the DS18B20 sensor
   pinMode(door_pin, INPUT); // door sensor as an input
   pinMode(led_pin, OUTPUT); // indicator led pin as an output
   digitalWrite(led_pin, LOW); // initialize indicator led in off
+
+  //GSM: Keep power when running from battery
+  Wire.begin(I2C_SDA, I2C_SCL);
+  bool isOk = setPowerBoostKeepOn(1);
+  SerialMon.println(String("IP5306 KeepOn ") + (isOk ? "OK" : "FAIL"));
+  //GSM Set modem reset, enable, power pins
+  pinMode(MODEM_PWKEY, OUTPUT);
+  pinMode(MODEM_RST, OUTPUT);
+  pinMode(MODEM_POWER_ON, OUTPUT);
+  digitalWrite(MODEM_PWKEY, LOW);
+  digitalWrite(MODEM_RST, HIGH);
+  digitalWrite(MODEM_POWER_ON, HIGH);
+  
+  // Set GSM module baud rate and UART pins
+  SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
+  delay(3000);
+  // Restart SIM800 module, it takes quite some time
+  // To skip it, call init() instead of restart()
+  SerialMon.println("Initializing modem...");
+  modem.restart();
+  //modem.init();
+  // use modem.init() if you don't need the complete restart
+
+  // Unlock your SIM card with a PIN if needed
+  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+    modem.simUnlock(simPIN);
+  }
 }
 
 ///Funciones
 void read_temperature(){
   tmp_sensor.requestTemperatures(); 
-  float temperatureC = tmp_sensor.getTempCByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
+  temperatureC = tmp_sensor.getTempCByIndex(0);
+  SerialMon.print(temperatureC);
+  SerialMon.println("ºC");
 }
 
 void read_distances(){
-/*   Serial.print(sonar_1.ping_cm());
-   Serial.println("cm 1");
-   Serial.print(sonar_2.ping_cm());
-   Serial.println("cm 2");
+  // ***AGREGAR SU CÓDIGO DEL SEGUNDO CARRIL DE LATAS usando cant_latas_sonar_2
+/*   SerialMon.print(sonar_1.ping_cm());
+   SerialMon.println("cm 1");
+   SerialMon.print(sonar_2.ping_cm());
+   SerialMon.println("cm 2");
 */
    dist_latas = sonar_1.ping_cm();
    if(dist_latas < 40){
-    cant_latas_sonar = 0;
+    cant_latas_sonar_1 = 0;
    }
    if(dist_latas < 35){
-    cant_latas_sonar = 1;
+    cant_latas_sonar_1 = 1;
    }
    if(dist_latas < 27){
-    cant_latas_sonar = 2;
+    cant_latas_sonar_1 = 2;
    }
    if(dist_latas < 19){
-    cant_latas_sonar = 3;
+    cant_latas_sonar_1 = 3;
    }
    if(dist_latas < 16){
-    cant_latas_sonar = 4;
+    cant_latas_sonar_1 = 4;
    }
    if(dist_latas < 6){
-    cant_latas_sonar = 5;
+    cant_latas_sonar_1 = 5;
    }
-   Serial.print(cant_latas_sonar);
-   Serial.println(" latas carril 4");
+   SerialMon.print(cant_latas_sonar_1);
+   SerialMon.println(" latas carril 4");
 }
 
 void read_door(){
   door_state = digitalRead(door_pin); // read the state of the door value
   if (door_state == HIGH) {
     digitalWrite(led_pin, LOW);
-    Serial.println("Puerta cerrada");
+    door_is = String("cerrada");
+    SerialMon.println("Puerta cerrada");
   } else {
     digitalWrite(led_pin, HIGH);
-    Serial.println("Puerta abierta");
+    door_is = String("abierta");
+    SerialMon.println("Puerta abierta");
   }
 }
 
 void read_cans(int switches_lane){
   switches_value = analogRead(switches_lane);
-  //Serial.print("switches_value = ");
-  //Serial.println(switches_value);
+  //SerialMon.print("switches_value = ");
+  //SerialMon.println(switches_value);
   if (switches_value < 4096){
     cant_latas = 0;
   }
@@ -109,18 +181,36 @@ void read_cans(int switches_lane){
   if (switches_value < 700){
     cant_latas = 4;
   }
-  Serial.print(cant_latas);
+  SerialMon.print(cant_latas);
   
+}
+void send_sms(){
+  String sms_message = String(" Temp: ") + String(temperatureC);
+  sms_message+= String("\n Latas1: ") + String(cant_latas_sonar_1);
+  sms_message+= String("\n Latas2: ") + String(cant_latas_sonar_1);
+  sms_message+= String("\n Puerta: ") + door_is;
+  sms_message+= String("\n Latas3: ") + String(cant_latas);
+  if(modem.sendSMS(SMS_TARGET,sms_message)){
+    SerialMon.println(sms_message);
+  }
+  else{
+    SerialMon.println("SMS failed to send");
+  }
 }
 
 /// Ciclo de ejecución
 void loop() {
-  Serial.println("Proyecto Inventarios");
+  SerialMon.println("Proyecto Inventarios");
   read_temperature();
   read_distances();
   read_door();
-  read_cans(cans_lane_1); Serial.println(" latas carril 1");
-  read_cans(cans_lane_2); Serial.println(" latas carril 2");
-  Serial.println();
+  read_cans(cans_lane_1); SerialMon.println(" latas carril 1");
+  read_cans(cans_lane_2); SerialMon.println(" latas carril 2");
+  SerialMon.println();
   delay(1000);
+  SerialMon.println("Triying to send SMS");
+  send_sms();
+  while(1){
+    
+  }
 }
