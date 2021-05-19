9c72e0312b8efa1411de61b1f82a5fc101fed7fb
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-05-19 18:54:04 -0500

    Adding comments
    After fully understanding the structure of timer interrupts, this version has explanation comments to avoid forgetting what is going on here

---
+++ Sandbox/Test_Timers/Test_Timers.ino
@@ -0,0 +1,37 @@
+volatile int interruptCounter = 0;
+int totalInterruptCounter;
+ 
+hw_timer_t * timer1 = NULL;
+portMUX_TYPE timer1Mux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer1() {
+  portENTER_CRITICAL_ISR(&timer1Mux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timer1Mux);
+}
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  timer1 = timerBegin(0, 80, true);                //Initialize timer. Timer#, Prescaler,true=rising count
+  timerAttachInterrupt(timer1, &onTimer1, true);    //Relates timer & ISR. Timer pointer,function pointer,edge
+  timerAlarmWrite(timer1, 1000000, true);          //Configures timer as an alarm. Timer pointer, uS value, reboot
+  timerAlarmEnable(timer1);                        //Enables the alarm (timer). Is it possible to disable an alarm?
+ 
+}
+ 
+void loop() {
+ 
+  if (interruptCounter > 0) {
+ 
+    portENTER_CRITICAL(&timer1Mux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timer1Mux);
+ 
+    totalInterruptCounter++;
+ 
+    Serial.print("An interrupt as occurred. Total number: ");
+    Serial.println(totalInterruptCounter);
+ 
+  }
+}
