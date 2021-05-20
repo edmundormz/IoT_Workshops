8bc7c0f9c06f88a01783188f003bee87fb9885ae
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-05-19 20:09:44 -0500

    Disabling timer alarm
    This version adds some lines at the end of the script to disable the timer alarm when a condition is true

--- Sandbox/Test_Timers/Test_Timers.ino
+++ Sandbox/Test_Timers/Test_Timers.ino
@@ -1,19 +1,20 @@
-volatile int interruptCounter = 0;
+volatile int interruptCounter1 = 0;
 int totalInterruptCounter;
  
 hw_timer_t * timer1 = NULL;
+
 portMUX_TYPE timer1Mux = portMUX_INITIALIZER_UNLOCKED;
 
 void IRAM_ATTR onTimer1() {
   portENTER_CRITICAL_ISR(&timer1Mux);
-  interruptCounter++;
+  interruptCounter1++;
   portEXIT_CRITICAL_ISR(&timer1Mux);
 }
  
 void setup() {
   Serial.begin(115200);
  
-  timer1 = timerBegin(0, 80, true);                //Initialize timer. Timer#, Prescaler,true=rising count
+  timer1 = timerBegin(3, 80, true);                //Initialize timer. Timer#, Prescaler,true=rising count
   timerAttachInterrupt(timer1, &onTimer1, true);    //Relates timer & ISR. Timer pointer,function pointer,edge
   timerAlarmWrite(timer1, 1000000, true);          //Configures timer as an alarm. Timer pointer, uS value, reboot
   timerAlarmEnable(timer1);                        //Enables the alarm (timer). Is it possible to disable an alarm?
@@ -22,10 +23,10 @@ void setup() {
  
 void loop() {
  
-  if (interruptCounter > 0) {
+  if (interruptCounter1 > 0) {
  
     portENTER_CRITICAL(&timer1Mux);
-    interruptCounter--;
+    interruptCounter1--;
     portEXIT_CRITICAL(&timer1Mux);
  
     totalInterruptCounter++;
@@ -33,5 +34,12 @@ void loop() {
     Serial.print("An interrupt as occurred. Total number: ");
     Serial.println(totalInterruptCounter);
  
+  }
+  if(totalInterruptCounter > 2){
+    timerAlarmDisable(timer1);
+    Serial.println("Alarm 1 Disabled");
+    while(1){
+      
+    }
   }
 }
