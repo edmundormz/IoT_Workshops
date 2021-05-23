b49592f09fd91cdeb088ef16690524b24594f537
H. Edmundo Ramírez <edmundormz@gmail.com>
2021-05-22 20:10:38 -0500

    Working code
    This code receives SMS to turn on/off a LED and now it also sends an SMS

--- Sandbox/TTGO_SMS_RELAY_AT/TTGO_SMS_RELAY_AT.ino
+++ Sandbox/TTGO_SMS_RELAY_AT/TTGO_SMS_RELAY_AT.ino
@@ -7,9 +7,9 @@
 
 #define SerialAT  Serial1
 
-String textMessage;
-String lampState = "HIGH";
-const int relay = 13;
+String textMessage = "";
+String lampState = "";
+const int blueLED = 13;
 
 void setup(){
   // Set modem reset, enable, power pins
@@ -20,29 +20,21 @@ void setup(){
   digitalWrite(MODEM_RST, HIGH);
   digitalWrite(MODEM_POWER_ON, HIGH);
 
-  pinMode(relay,OUTPUT);
-  digitalWrite(relay,HIGH);
+  pinMode(blueLED,OUTPUT);
+  digitalWrite(blueLED,LOW);
    
   SerialAT.begin(115200, SERIAL_8N1, MODEM_RX, MODEM_TX);
   delay(1000);
   Serial.begin(115200);               //Begin the Serial communication with PC
-  Serial.println("Starting ...");
   delay(5000);                      //Delay to let the module to connect to network, if turned on before you can remove this
   SerialAT.println("AT");            //Send AT command it will says "OK" if everything is fine
-  Serialcom();                      //description below
   SerialAT.print("AT+CMGF=1\r");     // Configuring TEXT mode
-  Serialcom();
   SerialAT.print("AT+CNMI=2,2,0,0,0\r"); // Set module to send SMS data to serial out upon receipt
-  Serialcom();
 }
 
 
 
 void loop() {
-  
-//Serialcom();                //The only function to run in loop, this function just check if the user has sent something using the Serial monitor and transfers it to the module
-                            //and if the module has something to say it will be displayed on the serial monitor, here used to show the feedback of the AT commands sent in the setup
-                            //and shows the received SMS
   
   if(SerialAT.available()>0){
     textMessage = SerialAT.readString();
@@ -51,17 +43,17 @@ void loop() {
     delay(10);
   }
   if(textMessage.indexOf("ON")>=0){
-    // Turn on relay and save current state
-    digitalWrite(relay, HIGH);
+    // Turn on blueLED and save current state
+    digitalWrite(blueLED, HIGH);
     lampState = "on";
-    Serial.println("Relay set to ON");  
+    Serial.println("blueLED set to ON");  
     textMessage = "";   
   }
   if(textMessage.indexOf("OFF")>=0){
-    // Turn off relay and save current state
-    digitalWrite(relay, LOW);
+    // Turn off blueLED and save current state
+    digitalWrite(blueLED, LOW);
     lampState = "off"; 
-    Serial.println("Relay set to OFF");
+    Serial.println("blueLED set to OFF");
     textMessage = ""; 
   }
   if(textMessage.indexOf("STATE")>=0){
@@ -70,7 +62,6 @@ void loop() {
     Serial.println("Lamp state resquest");
     textMessage = "";
   }
-  
 }
 
 // Function that sends SMS
@@ -78,25 +69,13 @@ void sendSMS(String message){
   // AT command to set SerialAT to SMS mode
   SerialAT.print("AT+CMGF=1\r");
   delay(100);
-  SerialAT.print("AT + CMGS = \"+523314173023\""); 
+  SerialAT.println("AT+CMGS=\"+523314173023\""); 
   delay(100);
-  SerialAT.println(message); 
+  SerialAT.print(message); 
   delay(100);
   // End AT command with a ^Z, ASCII code 26
   SerialAT.println((char)26); 
   delay(100);
   SerialAT.println();
-  delay(5000);  
-}
-
-void Serialcom(){
-  //delay(500);
-  while(Serial.available())                                                                      // IDE serial l serial  SerialAT
-  {
-    SerialAT.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while(SerialAT.available())                                                                      //serialSerialAT l serial dial IDE
-  {
-    Serial.write(SerialAT.read());//Forward what Software Serial received to Serial Port
-  }
+  delay(5000);
 }
